8d2b8da55a1cb9954d27b5d535b696ef102ebd16
Julia Wolf <juliahmartinez@yahoo.com>
2017-10-28 19:34:01 -0700

    updatedd

--- meow.js
+++ meow.js
@@ -2,17 +2,19 @@ window.onload = function() {
   console.log("Ready!!! MEOW!!!");
   var searchGiphyButton = document.getElementById("getMeow");
   var giphyImage = document.getElementById("rando-gliph-img");
-
+  var notYetText = document.getElementById("nothing-yet");
+  var searchInput = document.getElementById("textInput");
   searchGiphyButton.addEventListener("click", function() {
     console.log("Meow Click Click!!!");
-    fetchGiphy();
+    fetchGiphy(searchInput.value);
   });
 
-  function fetchGiphy() {
+  function fetchGiphy(str) {
     var xhr = new XMLHttpRequest();
     xhr.open(
       "GET",
-      "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=kitten"
+      "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=kitten" +
+        str
     );
     //console.log(XMLHttpRequest);
     xhr.onload = function() {
@@ -20,8 +22,9 @@ window.onload = function() {
         var payload = JSON.parse(xhr.response);
         console.log(payload.data.image_url);
         giphyImage.src = payload.data.image_url;
+        notYetText.setAttribute("class", "nothing-yet-hidden");
       }
+      xhr.send();
     };
-    xhr.send();
   }
 };
